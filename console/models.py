c604e64861a0cfaa7f66c1129112d349c96d2d4c
Adityaraj1711 <adityamailbox0@gmail.com>
2019-12-08 12:54:03 +0530

    string on model

--- console/models.py
+++ console/models.py
@@ -9,6 +9,8 @@ class Airports(models.Model):
     latitude = models.IntegerField()
     longitude = models.IntegerField()
 
+    def __str__(self):
+        return self.name + ' ' +self.code
 
 
 class Citizen(models.Model):
@@ -16,9 +18,16 @@ class Citizen(models.Model):
     age = models.IntegerField()
     passport_number = models.IntegerField()
 
+    def __str__(self):
+        self.name
+
+
 class Plane(models.Model):
     company = models.CharField(default='', max_length=50)
     plane_id = models.IntegerField()
+
+    def __str__(self):
+        return self.company
 
 
 class Flight(models.Model):
@@ -34,6 +43,9 @@ class Flight(models.Model):
     buisness_vacant = models.IntegerField()
     economy_vacant = models.IntegerField()
 
+    def __str__(self):
+        return 'from ' + self.flight_from.name + ' to ' + self.flight_to.name + ' on ' + str(self.time_of_boarding)
+
 
 class Booking(models.Model):
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
@@ -41,10 +53,16 @@ class Booking(models.Model):
     STATES = [(0, 'NA'), (1, 'FINISHED'), (2, 'INTRANSIT'), (3, 'CANCELLED'), (4, 'FUTURE')]
     state = models.IntegerField(choices=STATES, default=0)
 
+    def __str__(self):
+        return self.citizen.name + ' from ' + self.flight.flight_from.name
+
 
 class Luggage(models.Model):
     weight = models.IntegerField()
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.booking.citizen.name + ' have ' + str(self.weight)
 
 
 class Promotion(models.Model):
@@ -53,7 +71,13 @@ class Promotion(models.Model):
     discount_percent = models.IntegerField()
     valid = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name + ' applied with ' +str(self.discount_percent)
+
 
 class Transaction(models.Model):
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
     promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.booking.citizen.name
